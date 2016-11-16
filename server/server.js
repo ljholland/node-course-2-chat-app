7fe4158ce5361df6955bde2c6f7e3916b359c2a5
VM User <vmuser@bah.com>
2016-11-16 13:10:48 -0500

    Add listeners for messages

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
-const path = require ('path');
-const http = require ('http');
-const express = require ('express');
-const socketIO = require ('socket.io');
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -16,11 +14,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('disconnect', (socket) => {
-    console.log('User disconnected');
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
