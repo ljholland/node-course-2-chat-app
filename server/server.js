49b098d50c9b5e160413fc284b8a181dbba152eb
VM User <vmuser@bah.com>
2016-11-16 13:50:32 -0500

    Added new user messages

--- server/server.js
+++ server/server.js
@@ -13,7 +13,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    create: new Date().getTime()
+  })
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    create: new Date().getTime()
+  })
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -21,6 +30,11 @@ io.on('connection', (socket) => {
       text: message.text,
       create: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   create: new Date().getTime()
+    // })
   });
 
   socket.on('disconnect', () => {
