b58559073845e5e9c30ffa8ab289bd944f6601e1
VM User <vmuser@bah.com>
2016-11-16 22:16:57 -0500

    Added goelocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-  it ('should generate the correct message object', () => {
-     const email = 'lance@LOL.com';
-     const textMsg = 'some exciting text';
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
 
-     var message = generateMessage(email,textMsg);
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});
 
-     expect(message.createdAt).toBeA('number');
-     expect(message).toInclude({from, textMsg});
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
