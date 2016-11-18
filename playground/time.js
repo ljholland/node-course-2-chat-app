38833d0bce8c7e47cc5ea372fdcd3f097ae03abe
VM User <vmuser@bah.com>
2016-11-18 14:06:51 -0500

    format timestamps using moment

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+// var createdAt = moment().valueOf();
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+// var date = moment().valueOf();
+
+//var date = moment();
+
+// console.log(date.format(`MMM Do, YYYY h:mm:ssa`));
